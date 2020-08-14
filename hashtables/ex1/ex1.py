63fc4e1e072659038dc63c05d1ffd7fa5babadc5
Daniel Martin <danielmartin1205@gmail.com>
2020-08-14 19:28:25 -0400

    2 tests passing

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,23 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+    lookup = {}
 
-    return None
+    ans = None
+    for i in range(length):
+        lookup[weights[i]] = i
+    
+    for i in range(length):
+        search = (limit - weights[i])
+        if search in lookup:
+            # it's almost guaranteed to see the smallest index first..
+            # biggest = max(lookup[search],i)
+            # smallest = min(lookup[search], i)
+            ans = (lookup[search],i)
+            break
+
+
+    return ans
+
+
+print(get_indices_of_item_weights([ 4, 6, 10, 15, 16 ], 5, 21))
